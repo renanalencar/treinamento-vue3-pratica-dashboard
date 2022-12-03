ea0d5b11183e4f0b9f1f0db8713587f808630cdc
Renan Costa Alencar <renan.costaalencar@gmail.com>
2022-12-03 18:02:40 -0300

    Criando tela de credenciais

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,37 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+  <modal-factory />
+  <router-view />
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import { watch } from 'vue'
+import ModalFactory from './components/ModalFactory'
+import { useRouter, useRoute } from 'vue-router'
+import services from './services'
+import { setCurrentUser } from './store/user'
 
-#nav {
-  padding: 30px;
-}
+export default {
+  components: { ModalFactory },
+  setup () {
+    const router = useRouter()
+    const route = useRoute()
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+    watch(
+      () => route.path,
+      async () => {
+        if (route.meta.hasAuth) {
+          const token = window.localStorage.getItem('token')
+          if (!token) {
+            router.push({ name: 'Home' })
+            return
+          }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
+          const { data } = await services.users.getMe()
+          setCurrentUser(data)
+          console.log('data', data)
+        }
+      }
+    )
+  }
 }
-</style>
+</script>
---
+++ src/components/Icon/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <component :is="name" v-bind="$props"/>
+</template>
+
+<script>
+import Loading from './Loading.vue'
+import Copy from './Copy.vue'
+export default {
+  components: { Loading, Copy },
+  props: {
+    name: { type: String, required: true }
+  }
+}
+</script>
